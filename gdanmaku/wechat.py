e84bce45e4365ab0e5c09014bbe49e6ea7ba93b5
bigeagle <justin.w.xd@gmail.com>
2015-01-14 18:43:54 +0800

    modify wechat command and prettify code

--- gdanmaku/wechat.py
+++ gdanmaku/wechat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python2
 # -*- coding:utf-8 -*-
-import json
 from flask import current_app, request, g, Response, make_response
 from . import app
 import xml.etree.ElementTree as ET
@@ -9,19 +8,14 @@ import hashlib
 import time
 
 
-def jsonResponse(r):
-    return Response(json.dumps(r), mimetype='application/json')
-
-
 @app.route("/api/wechat", methods=["GET", "POST"])
 def api_wechat_handle():
-    if wechat_auth() is False:
-        return "BAD TOKEN"
+    if not wechat_verify():
+        return "BAD TOKEN", 400
 
     if request.method == 'GET':
         echostr = request.args.get('echostr', '')
-        if echostr is not '':
-            return echostr
+        return echostr
 
     xml_recv = ET.fromstring(request.data)
     ToUserName = xml_recv.find("ToUserName").text
@@ -31,7 +25,7 @@ def api_wechat_handle():
     cm = g.channel_manager
 
     # 加入频道
-    matchjoin = re.compile(ur'^加入(?:\s)*(\S+)(?:\s)*(\S+)?')
+    matchjoin = re.compile(ur'^[:：]加入(?:\s)+(\S+)(?:\s)+(\S+)?')
     match = matchjoin.split(Content)
 
     if len(match) > 2:
@@ -39,54 +33,54 @@ def api_wechat_handle():
             return make_reply(
                 FromUserName,
                 ToUserName,
-                '命令错误哦，回复"帮助"看看使用说明吧')
+                u'命令错误哦，回复"帮助"看看使用说明吧')
 
         channel = cm.get_channel(match[1])
         if channel is None:
-            return make_reply(FromUserName, ToUserName, "木有这个频道。。。")
+            return make_reply(FromUserName, ToUserName, u"木有这个频道。。。")
 
         if channel.is_open:
             g.r.set(''.join(['wechat.', FromUserName, '.ch_name']), match[1])
-            return make_reply(FromUserName, ToUserName, "加入成功")
+            return make_reply(FromUserName, ToUserName, u"加入成功")
 
         # 加密频道
         if match[2] is None or (not channel.verify_pub_passwd(match[2])):
-            return make_reply(FromUserName, ToUserName, "密码不对。。在试试？")
+            return make_reply(FromUserName, ToUserName, u"密码不对。。在试试？")
 
         g.r.set(''.join(['wechat.', FromUserName, '.ch_name']), match[1])
         g.r.set(''.join(['wechat.', FromUserName, '.ch_key']), match[2])
-        return make_reply(FromUserName, ToUserName, "设置通道成功，发射吧")
+        return make_reply(FromUserName, ToUserName, u"设置通道成功，发射吧")
 
     # 设置弹幕属性
-    matchsetting = re.compile(ur"^设置(?:\s)*(\S+)(?:\s)*(\S+)?")
+    matchsetting = re.compile(ur"^[:：]设置(?:\s)*(\S+)(?:\s)*(\S+)?")
     match = matchsetting.split(Content)
     if len(match) > 2:
         position, color = option_trans(match[1], match[2])
         if position is None:
             return make_reply(
-                FromUserName, ToUserName, "命令错误哦，回复帮助看看使用说明吧")
+                FromUserName, ToUserName, u"命令错误哦，回复帮助看看使用说明吧")
 
         g.r.set(''.join(['wechat.', FromUserName, '.ch_pos']), position)
         if color is not None:
             g.r.set(''.join(['wechat.', FromUserName, '.ch_color']), color)
 
-        return make_reply(FromUserName, ToUserName, "设置成功，发射吧！")
+        return make_reply(FromUserName, ToUserName, u"设置成功，发射吧！")
 
     # 帮助
-    matchhelp = re.compile(ur"^[帮助|help|Help]")
+    matchhelp = re.compile(ur"^[:：][帮助|help]", re.IGNORECASE)
     match = matchhelp.match(Content)
     if match:
         return make_reply(
             FromUserName,
             ToUserName,
-            ("来发射弹幕吧！\n"
-             "回复 加入+频道名称+发射密码 加入频道\n"
-             "如“加入 sheyifa 123456”\n"
-             "加入开放频道则密码留空\n"
-             "回复 设置+位置+颜色 设置弹幕属性\n"
-             "如“设置 顶部 白”\n"
-             "可选的位置有：飞过 顶部 底部\n"
-             "可选的颜色有：蓝 白 红 黄 青 绿 紫 黑")
+            (u"来发射弹幕吧！\n"
+             u"回复 \":加入 频道名称 [发射密码]\" 加入频道\n"
+             u"如\":加入 sheyifa 123456\"\n"
+             u"加入开放频道则密码留空\n"
+             u"回复 \":设置 位置 [颜色]\" 设置弹幕属性\n"
+             u"如 \":设置 顶部 白\" \n"
+             u"可选的位置有：飞过 顶部 底部\n"
+             u"可选的颜色有：蓝 白 红 黄 青 绿 紫 黑")
         )
 
     ch_name = g.r.get(''.join(['wechat.', FromUserName, '.ch_name']))
@@ -96,55 +90,46 @@ def api_wechat_handle():
 
     if ch_name is None:
         return make_reply(
-            FromUserName, ToUserName, "还没有加入频道或者超时了 T_T")
+            FromUserName, ToUserName, u"还没有加入频道或者超时了 T_T")
 
     channel = cm.get_channel(ch_name)
     if ch_key is None:
         ch_key = ''
     if channel is None:
-        return make_reply(FromUserName, ToUserName, "频道已經关闭了 T_T")
+        return make_reply(FromUserName, ToUserName, u"频道已經关闭了 T_T")
 
     if not channel.is_open and not channel.verify_pub_passwd(ch_key):
-        return make_reply(FromUserName, ToUserName, "密码错误 T_T")
-
-    if ch_pos is None:
-        ch_pos = 'fly'
-    if ch_color is None:
-        ch_color = 'white'
+        return make_reply(FromUserName, ToUserName, u"密码错误 T_T")
 
     danmaku = {
         "text": Content,
-        "style": ch_color,
-        "position": ch_pos,
+        "style": ch_color or 'fly',
+        "position": ch_pos or 'blue',
     }
     channel.new_danmaku(danmaku)
 
-    return make_reply(FromUserName, ToUserName, "发射成功！")
+    return make_reply(FromUserName, ToUserName, u"发射成功！")
 
 
-def wechat_auth():
+def wechat_verify():
     token = current_app.config.get("WECHAT_TOKEN")  # your token
     query = request.args  # GET 方法附上的参数
     signature = query.get('signature', '')
     timestamp = query.get('timestamp', '')
     nonce = query.get('nonce', '')
-    s = [timestamp, nonce, token]
-    s.sort()
-    s = ''.join(s)
-    if (hashlib.sha1(s).hexdigest() == signature):
-        return True
-    return False
+    s = ''.join(sorted([timestamp, nonce, token]))
+    return hashlib.sha1(s).hexdigest() == signature
 
 
 def make_reply(touser, fromuser, content):
-    tmpl = ("<xml>"
-            "<ToUserName><![CDATA[{touser}]]></ToUserName>"
-            "<FromUserName><![CDATA[{fromuser}]]></FromUserName>"
-            "<CreateTime>{ts}</CreateTime>"
-            "<MsgType><![CDATA[text]]></MsgType>"
-            "<Content><![CDATA[{content}]]></Content>"
-            "<FuncFlag>0</FuncFlag>"
-            "</xml>")
+    tmpl = (u"<xml>"
+            u"<ToUserName><![CDATA[{touser}]]></ToUserName>"
+            u"<FromUserName><![CDATA[{fromuser}]]></FromUserName>"
+            u"<CreateTime>{ts}</CreateTime>"
+            u"<MsgType><![CDATA[text]]></MsgType>"
+            u"<Content><![CDATA[{content}]]></Content>"
+            u"<FuncFlag>0</FuncFlag>"
+            u"</xml>")
 
     response = make_response(
         tmpl.format(
