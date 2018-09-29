b91f3fe711d9165af5822e347c1ccc0fb666b061
berry <berrywang1997@me.com>
2018-09-29 19:38:48 +0800

    changed settings instead of output info since it is unrealistic to expect both server and clients have access to ipv6 network

--- gdanmaku/__init__.py
+++ gdanmaku/__init__.py
@@ -51,8 +51,8 @@ from . import api
 from . import wechat
 
 def main():
-    http_server = WSGIServer(('', 5000), app)
-    print("Serving at localhost:5000")
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    print("Serving at 0.0.0.0:5000")
     http_server.serve_forever()
 
 
